5b06ed2f5019ddc64e0f304641a83b98108f83cb
vickyruud <aprajit.k@gmail.com>
2021-08-26 14:05:17 -0400

    commit after initial file setup

--- server.js
+++ server.js
@@ -7,8 +7,28 @@ const morgan = require("morgan");
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));// activating body parser for POST requests
 
-app.get("/", (res, req) => {
-  res.setEncoding("You have connected!!");
-})
+app.set("view engine", "ejs");
+
+const songs = {
+  1000 : {
+    id : 1000,
+    name: "Time",
+    Artist : "Hans Zimmer"
+  },
+  2000 : {
+    id : 2000,
+    name: "Ezio's Family",
+    Artist : "Jesper Kyd"
+  },
+};
+
+app.get("/", (req, res) => {
+  res.send(songs);
+});
+
+app.get("/songs", (req, res) => {
+  res.render("songs_index");
+});
+
 
 app.listen(PORT, () => console.log("Awaiting connection on: ", PORT));
